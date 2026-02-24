2907eee497ecec3c261e0da586422324cffa2f60
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2026-02-24 19:23:00 +0700

    Add ping command to respond with PONG message

---
+++ plugins/ping.py
@@ -0,0 +1,6 @@
+from kurigram import filters, Client
+
+@Client.on_message(filters.command("ping", ".") & filters.me)
+async def ping_ubot(client, message):
+    await message.edit("PONG! Userbot Aktif 🔥")
+  
